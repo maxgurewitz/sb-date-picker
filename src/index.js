b23685582b213bbb9c8310450d3dd8b1d1d97088
Max Gurewitz <maxthegeek1@gmail.com>
2015-11-26 00:03:12 -0800

    basic code structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function main() {
-  return 'hello world';
+var render = require('./renderers/date-picker');
+var update = require('./update');
+var buildModel = require('./build-model');
+var buildActionEmitter = require('./build-action-emitter');
+
+module.exports = {
+  render: render,
+  update: update,
+  buildModel: buildModel,
+  buildActionEmitter: buildActionEmitter
 };
